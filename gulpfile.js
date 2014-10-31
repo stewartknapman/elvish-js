f6e67af15c5e16bdb1aa5c4651037e076d00fada
Stewart Knapman <info@stewartknapman.com>
2014-10-31 15:07:53 +1300

    its working!

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 var gulp        = require('gulp'),
   gutil         = require('gulp-util'),
   plumber       = require('gulp-plumber'),
+  source        = require('vinyl-source-stream'),
+  browserify    = require('browserify'),
+  jshint        = require('gulp-jshint'),
   sass          = require('gulp-ruby-sass'),
   prefix        = require('gulp-autoprefixer');
   
@@ -11,6 +14,7 @@ var onError = function (err) {
   
   
 gulp.task('default', function () {
+  gulp.watch('./js/app.js', ['lint', 'browserify']);
   gulp.watch('./css/*.scss', ['sass']);
 });
 
@@ -24,4 +28,24 @@ gulp.task('sass', function () {
     }))
     .pipe(prefix("last 2 versions", "> 5%", "ie 8", "ie 7"))
     .pipe(gulp.dest('./css/'));
+});
+
+gulp.task('lint', function () {
+  return gulp.src('./js/app.js')
+    .pipe(plumber({
+      errorHandler: onError
+    }))
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+gulp.task('browserify', function () {
+  return browserify('./js/app.js', {
+      debug: true                        // with source maps
+    })
+    .transform('brfs')                   // include file system
+    .transform({ global: true }, 'uglifyify')
+    .bundle()
+    .pipe(source('script.js'))             // Pass desired output filename to vinyl-source-stream
+    .pipe(gulp.dest('./js/'));   // Start piping stream to tasks!
 });
--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
+var $ = require('jquery');
+var tengwar = require('tengwar/general-use');
+var fonts = require("tengwar/fonts");
+
 $(document).on('keyup', function(e){
   var $focus = $(document.activeElement);
   var $source = $('.translation-source');
@@ -5,8 +9,17 @@ $(document).on('keyup', function(e){
   if($focus[0] === $source[0]){
     var target = $source.data('target');
     var $target = $(target);
-    var new_text = $source.val();
-    $target.text(new_text);
+    var text = $source.val();
+    
+    var elvish = tengwar.transcribe(text, {
+      font: fonts['annatar'],
+      language: null
+    });
+    
+    $target.text(elvish);
   }
-  
+});
+
+$(function () {
+  $('.translation-source').focus();
 });
