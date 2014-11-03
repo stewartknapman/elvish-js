f143455fb6475780486164401161bb7aced54cbc
Stewart Knapman <info@stewartknapman.com>
2014-11-04 08:48:08 +1300

    tracking down stuff

--- js/app.js
+++ js/app.js
@@ -10,12 +10,14 @@ $(document).on('keyup', function(e){
     var target = $source.data('target');
     var $target = $(target);
     var text = $source.val();
+    console.log('Elvish input:', text);
     
     var elvish = tengwar.transcribe(text, {
       font: fonts.annatar,
       noAchLaut: true,
       language: null
     });
+    console.log('Elvish output:', elvish);
     
     $target.html(elvish);
   }
