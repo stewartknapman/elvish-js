78c0f4974e788129a1d8a132b27a572bef91713e
Stewart Knapman <info@stewartknapman.com>
2015-04-08 12:15:28 +1200

    classify

--- js/app.js
+++ js/app.js
@@ -2,19 +2,37 @@ var $ = require('jquery');
 var tengwar = require('tengwar/general-use');
 var fonts = require("tengwar/fonts");
 
-var init_translation = function () {
-  var focus = $(document.activeElement)[0];
-  $('.translation-source').each(function () {
-    translate(this, focus);
+var ElvishTranslator = function (translation_source) {
+  this.translation_source = $(translation_source);
+  
+  this.add_event_listeners();
+  this.init_translation();
+};
+
+ElvishTranslator.prototype.get_focused = function () {
+  return $(document.activeElement)[0];
+};
+
+ElvishTranslator.prototype.add_event_listeners = function () {
+  var _this = this;
+  $(document).on('keyup', function(e){
+    _this.translate();
   });
 };
 
-var translate = function (source, focus) {
-  if(focus === source){
-    var $source = $(source);
-    var target = $source.data('target');
+ElvishTranslator.prototype.init_translation = function () {
+  if (this.translation_source.hasClass('translate-on-load') && this.translation_source.val() !== '') {
+    this.translation_source.focus();
+    this.translate();
+  }
+};
+
+ElvishTranslator.prototype.translate = function () {
+  var focus = this.get_focused();
+  if(focus === this.translation_source[0]){
+    var target = this.translation_source.data('target');
     var $target = $(target);
-    var text = $source.val();
+    var text = this.translation_source.val();
     
     var elvish = tengwar.transcribe(text, {
       font: fonts.annatar,
@@ -26,14 +44,8 @@ var translate = function (source, focus) {
   }
 };
 
-$(document).on('keyup', function(e){
-  init_translation();
-});
-
 $(function () {
-  $('.translation-source').focus();
-  if ($('.translation-source').hasClass('translate-on-load')) {
-    console.log('translate on load');
-    init_translation();
-  }
+  $('.translation-source').each(function () {
+    new ElvishTranslator(this);
+  });
 });
